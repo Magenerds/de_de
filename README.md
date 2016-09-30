6f3bd35cdbd33351d3ab2cd3a164c6870c39043f
Florian Sydekum <f.sydekum@techdivision.com>
2016-09-30 08:35:38 +0200

    Initial commit of module

---
+++ README.md
@@ -0,0 +1,5 @@
+# Magenerds_de_de
+
+Website: http://www.magenerds.com/magento-2-extensions/german-language-pack/
+
+Documentation: http://doc.magenerds.com/german-language-pack/
---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @category   Magenerds
+ * @package    magenerds_de_de
+ * @subpackage Module
+ * @copyright  Copyright (c) 2016 TechDivision GmbH (http://www.techdivision.com)
+ * @version    ${release.version}
+ * @link       http://www.techdivision.com/
+ * @author     Florian Sydekum <f.sydekum@techdivision.com>
+ * @license    OSL 3.0
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magenerds_de_de',
+    __DIR__
+);
